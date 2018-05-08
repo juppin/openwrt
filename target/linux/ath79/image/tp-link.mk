1a7a2be6384b79cfb8fc8e6f7330560524397bcf
Johann Neuhauser <johann@it-neuhauser.de>
2018-05-08 15:49:31 +0200

    ath79: add preliminary support for tp-link wr1043 v2/v3
    Signed-off-by: Johann Neuhauser <johann@it-neuhauser.de>

--- target/linux/ath79/image/tp-link.mk
+++ target/linux/ath79/image/tp-link.mk
@@ -1,3 +1,5 @@
+include ./legacy.mk
+
 DEVICE_VARS += TPLINK_HWID TPLINK_HWREV TPLINK_FLASHLAYOUT TPLINK_HEADER_VERSION TPLINK_BOARD_NAME
 
 # combine kernel and rootfs into one image
@@ -58,12 +60,11 @@ define Device/tplink
   TPLINK_HWREV := 0x1
   TPLINK_HEADER_VERSION := 1
   LOADER_TYPE := gz
-  KERNEL := kernel-bin | patch-cmdline | lzma
-  KERNEL_INITRAMFS := kernel-bin | patch-cmdline | lzma | mktplinkfw-combined
-#  IMAGES := sysupgrade.bin
+  KERNEL := kernel-bin | append-dtb | lzma
+  KERNEL_INITRAMFS := kernel-bin | append-dtb | lzma
   IMAGES := sysupgrade.bin factory.bin
   IMAGE/sysupgrade.bin := append-rootfs | mktplinkfw sysupgrade
-#  IMAGE/factory.bin := append-rootfs | mktplinkfw factory | a
+  IMAGE/factory.bin := append-rootfs | mktplinkfw factory
 endef
 
 define Device/tplink-nolzma
@@ -117,3 +118,22 @@ define Device/tl_wr1043nd_v1
 endef
 
 #TARGET_DEVICES += tl_wr1043nd_v1
+
+define Device/tl_wr1043nd_v2
+  $(Device/tplink-8mlzma)
+  ATH_SOC := qca9558
+  DEVICE_TITLE := TP-LINK TL-WR1043N/ND v2
+  DEVICE_PACKAGES := kmod-usb-core kmod-usb2 kmod-usb-ledtrig-usbport
+  BOARDNAME := TL-WR1043ND-v2
+  DEVICE_PROFILE := TLWR1043
+  TPLINK_HWID := 0x10430002
+endef
+TARGET_DEVICES += tl_wr1043nd_v2
+
+define Device/tl_wr1043nd_v3
+  $(Device/tl_wr1043nd_v2)
+  DEVICE_TITLE := TP-LINK TL-WR1043N/ND v3
+  TPLINK_HWID := 0x10430003
+endef
+TARGET_DEVICES += tl_wr1043nd_v3
+
